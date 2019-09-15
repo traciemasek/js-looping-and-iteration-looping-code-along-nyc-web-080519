23cb33c41290798b0c1906e4314ae2b4942b60c6
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-09-15 12:06:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards = (names, event) => {
+  const thankYouCards = []
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  };
+  return thankYouCards;
+};
+
+const countdown = (int) => {
+  while (int >= 0) {
+    console.log(int);
+    int--;
+  }
+};
